da2a55315f52560371b7d5573f37ee7773b9970c
Mikołaj Rosowski <m.rosowski1@wp.pl>
2021-09-01 18:36:31 +0200

    fix: fixed blurry linesin canvas drawGrid

--- src/main.ts
+++ src/main.ts
@@ -39,13 +39,13 @@ async function main() {
     ctx.strokeStyle = GRID_COLOR;
 
     for (let i = 0; i <= width; i++) {
-      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+      ctx.moveTo(i * (CELL_SIZE + 1) + 0.5, 0);
+      ctx.lineTo(i * (CELL_SIZE + 1) + 0.5, (CELL_SIZE + 1) * height + 1);
     }
 
     for (let j = 0; j <= height; j++) {
-      ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+      ctx.moveTo(0, j * (CELL_SIZE + 1) + 0.5);
+      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 0.5);
     }
 
     ctx.stroke();
